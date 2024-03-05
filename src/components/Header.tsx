1af4172efbc8e353b0c3a22a04401d1ceb7db1d8
miju <im.ymiju@gmail.com>
2024-03-05 21:12:49 +0900

    Feat: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="w-full h-28 flex items-center justify-center border-b border-yellofont ">
+      <h1 className="font-bold text-5xl text-yellowfont">⭐TODO⭐</h1>
+    </header>
+  );
+}
